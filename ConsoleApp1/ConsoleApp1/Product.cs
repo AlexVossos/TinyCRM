b11e5f0e26e2d62855c2b67356338a845ccb951d
ΑΛΕΞΑΝΔΡΟΣ ΙΩΑΝΝΗΣ ΒΩΣΣΟΣ <avossos@athtech.gr>
2020-04-30 20:01:45 +0300

    Added product file parsing.

---
+++ ConsoleApp1/ConsoleApp1/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    public class Product
+    {
+        public string ProductID { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Product(string productID, string name, string description, decimal price)
+        {
+            this.ProductID = productID;
+            this.Name = name;
+            this.Description = description;
+            this.Price = price;
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace ConsoleApp1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = @"C:\Users\goodm\devel\TinyCRMGit\ConsoleApp1\ConsoleApp1\products.csv";
+            string[] lines = File.ReadAllLines(path);
+            decimal price = 0M;            
+            lines = lines.Skip(1).ToArray();
+            List<Product> ProductList = new List<Product>();
+            foreach(string line in lines)
+            {
+                var values = line.Split(';');
+                price = GetRandomDecimal();
+                if (!(ProductList.Any(item => item.ProductID == values[0]))) ;
+                {
+                    ProductList.Add(new Product(values[0], values[1], values[2], price));
+                }                
+            }
+            Console.WriteLine("Done");
+        } 
+        
+        public static decimal GetRandomDecimal()
+        {
+            var random = new Random();
+            var randomNumber = random.NextDouble() * 100;
+            return (decimal)Math.Round(randomNumber, 2);
+            
         }
     }
 }
